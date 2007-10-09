8b4d0e81099f90eae40e18967324e36f01c1f214
Björn Dieding <bjoern@xrow.de>
2007-10-09 19:59:46 +0000

    some more bugfixes
    git-svn-id: http://svn.projects.ez.no/recurringorders@30 db830d13-330c-426f-b442-160563b850f0

--- trunk/modules/recurringorders/add.php
+++ trunk/modules/recurringorders/add.php
@@ -17,9 +17,10 @@ include_once( 'lib/ezutils/classes/ezhttptool.php' );
 include_once( 'extension/recurringorders/classes/recurringordercollection.php');
 if ( $http->hasPostVariable( "ActionAddToRecurring" ) )
 {
+    $result = array();
     if ( $http->hasPostVariable( "AddToBasketList" ) and is_array( $http->postVariable( "AddToBasketList" ) ) )
     {
-        $result = array();
+        
         foreach ( $http->postVariable( "AddToBasketList" ) as $position )
         {
             if ( $position['quantity'] > 0 and $position['object_id'] )
@@ -29,24 +30,43 @@ if ( $http->hasPostVariable( "ActionAddToRecurring" ) )
                 $result[] = $position;
             }
         }
-        if ( count( $result ) == 0 )
-        {
-             $module->redirectTo( $_SERVER['HTTP_REFERER'] );
-             return;
-        }
-        $collection  = XROWRecurringOrderCollection::fetchByUser();
-        if ( !is_array( $collection ) or count( $collection ) == 0 )
-            $collection = XROWRecurringOrderCollection::createNew();
-        elseif ( is_array( $collection ) )
-        {
-            $collection = $collection[0];
-        }
-        foreach ( $result as $item )
-        {
-            $collection->add( $item['object_id'], $item['variations'], $item['quantity']);
-        }
-        $http->setSessionVariable( "RedirectURI", $http->sessionVariable( 'LastAccessesURI' ) );
     }
+    elseif ( $http->hasPostVariable( "ContentObjectID" ) and $http->hasPostVariable( "ProductItemCountList" ) )
+    {
+        $result = array();
+        $position = array();
+        if ( $http->postVariable( "ProductItemCountList" ) > 0 )
+        {
+            $position['quantity'] = $http->postVariable( "ProductItemCountList" );
+        }
+        else
+        {
+            $position['quantity'] = 1;
+        }
+        $position['object_id'] = $http->postVariable( "ContentObjectID" );
+        $position['variations'] = array();
+        $result[] = $position;
+    }
+    if ( count( $result ) == 0 )
+    {
+        $module->redirectTo( $_SERVER['HTTP_REFERER'] );
+        return;
+    }
+    $collection  = XROWRecurringOrderCollection::fetchByUser();
+    if ( !is_array( $collection ) or count( $collection ) == 0 )
+    $collection = XROWRecurringOrderCollection::createNew();
+    elseif ( is_array( $collection ) )
+    {
+        $collection = $collection[0];
+    }
+    foreach ( $result as $item )
+    {
+        $collection->add( $item['object_id'], $item['variations'], $item['quantity']);
+    }
+    $http->setSessionVariable( "RedirectURI", $http->sessionVariable( 'LastAccessesURI' ) );
 }
-return $Module->redirectTo( "recurringorders/list/" . $collection->id );
+if ( $collection->id )
+    return $Module->redirectTo( "recurringorders/list/" . $collection->id );
+else
+    return $Module->handleError( EZ_ERROR_KERNEL_ACCESS_DENIED, 'kernel' );
 ?>
