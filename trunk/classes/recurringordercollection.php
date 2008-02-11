d0b6df5d672ad19fc4ef2505d20ea3b6f4f75f42
Björn Dieding <bjoern@xrow.de>
2008-02-11 19:59:30 +0000

    git-svn-id: http://svn.projects.ez.no/recurringorders@40 db830d13-330c-426f-b442-160563b850f0

--- trunk/classes/recurringordercollection.php
+++ trunk/classes/recurringordercollection.php
@@ -76,8 +76,8 @@ class XROWRecurringOrderCollection extends eZPersistentObject
     function now()
     {
         #$time = 1189002190;
-        #$time = gmmktime( 0,0,0,2,5,2008 );
-        $time = gmmktime( 0,0,0 );
+        $time = gmmktime( 0,0,0,6,5,2008 );
+        #$time = gmmktime( 0,0,0 );
         return $time;
     }
     function markRun()
@@ -116,6 +116,10 @@ class XROWRecurringOrderCollection extends eZPersistentObject
             {
                 return true;
             }
+        }
+        elseif ( $data[XROWCREDITCARD_KEY_TYPE] == XROWCREDITCARD_TYPE_EUROCARD )
+        {
+            return true;
         }
         else
         {
